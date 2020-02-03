c6433031b23fd55bdd7745095103851cef1329ff
hannah <hholl002@odu.edu>
2020-02-03 18:56:37 -0500

    commit

---
+++ app/src/main/java/edu/odu/cs411/loqui/Homepage.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:79eefdfc14380e9bd6495969dc030f6c6cb11a67219bf642cffec0261c914bfb
+size 2230
---
+++ app/src/main/java/edu/odu/cs411/loqui/ImageProcessor.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d1f67577dae24524b3c7887003be3d9220f998e63684c4ada988bf39e5a2672a
+size 1346
