09b0d0e23a08fac94ea7f26035ad143de36d1b59
hannah <hholl002@odu.edu>
2020-02-03 18:51:57 -0500

    commit

--- app/src/main/java/edu/odu/cs411/loqui/MainActivity.java
+++ app/src/main/java/edu/odu/cs411/loqui/MainActivity.java
@@ -1,13 +1,38 @@
 package edu.odu.cs411.loqui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import android.support.v7.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button enter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
         setContentView(R.layout.activity_main);
+        clickOnButton();
+    }
+
+    private void clickOnButton(){
+        enter = (Button) findViewById(R.id.enter);
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, Homepage.class);
+                startActivity(it);
+            }
+        });
     }
 }
+
