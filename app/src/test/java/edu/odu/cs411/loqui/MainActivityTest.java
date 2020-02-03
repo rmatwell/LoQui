2e3d8bd6c9d9484d8e7496e3bfc810db15ad3b5d
rmatwell <richard.atwell@hotmail.com>
2020-02-02 21:39:56 -0500

    Updated gradle.build file and applicationID to edu.odu.cs411.loqui

---
+++ app/src/test/java/edu/odu/cs411/loqui/MainActivityTest.java
@@ -0,0 +1,14 @@
+package edu.odu.cs411.loqui;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
