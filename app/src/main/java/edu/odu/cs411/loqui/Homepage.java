8b205932c49bbf8b565db11425abeddd4d45a959
hannah <hholl002@odu.edu>
2020-02-03 18:56:37 -0500

    commit

---
+++ app/src/main/java/edu/odu/cs411/loqui/Homepage.java
@@ -0,0 +1,61 @@
+package edu.odu.cs411.loqui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.provider.DocumentsProvider;
+
+public class Homepage extends AppCompatActivity {
+
+    private Button go1, go2, go3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+        setContentView(R.layout.activity_homepage);
+        clickOnButton();
+    }
+
+    private void clickOnButton(){
+        go1 = (Button) findViewById(R.id.go1);
+        go2 = (Button) findViewById(R.id.go2);
+        go3 = (Button) findViewById(R.id.go3);
+        go1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(Homepage.this, IntroPage.class);
+                it.putExtra("content", "Look at the pictures, and learn what they mean!");
+                it.putExtra("task", "Learn Your Emotions");
+                it.putExtra("pic_id", R.drawable.task1_intro);
+                startActivity(it);
+            }
+        });
+        go2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(Homepage.this, IntroPage.class);
+                it.putExtra("content", "Make your face look like the picture of the character!");
+                it.putExtra("task", "Face Matching Game");
+                it.putExtra("pic_id", R.drawable.task2_intro);
+                startActivity(it);
+            }
+        });
+        go3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(Homepage.this, IntroPage.class);
+                it.putExtra("content", "Lets see how well you know your emotions!");
+                it.putExtra("task", "Emotion Quiz");
+                it.putExtra("pic_id", R.drawable.task3_intro);
+                startActivity(it);
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/edu/odu/cs411/loqui/ImageProcessor.java
@@ -0,0 +1,53 @@
+package edu.odu.cs411.loqui;
+
+import com.microsoft.projectoxford.face.FaceServiceRestClient;
+
+public class ImageProcessor {
+
+
+//    public static Bitmap drawRectFace (Bitmap bitmap, FaceRectangle faceRectangle, String status) {
+//
+//        Bitmap bitmap1 = bitmap.copy(Bitmap.Config.ARGB_8888,true);
+//        Canvas canvas = new Canvas(bitmap1);
+//        Paint paint = new Paint();
+//        paint.setAntiAlias(true);
+//        paint.setStyle(Paint.Style.STROKE);
+//        paint.setColor(Color.WHITE);
+//        paint.setStrokeWidth(12);
+//
+//
+//        canvas.drawRect(faceRectangle.left,
+//                faceRectangle.top,
+//                faceRectangle.left + faceRectangle.width,
+//                faceRectangle.top + faceRectangle.height,
+//                paint);
+//
+//        int deltaX = faceRectangle.left + faceRectangle.width;
+//        int deltaY = faceRectangle.top + faceRectangle.height;
+//
+//        drawTextOnBitMap(canvas,100,deltaX/2 + deltaX/5, deltaY + 100, Color.WHITE, status);
+//
+//
+//
+//    }
+//
+//
+//
+//    private static void drawTextOnBitMap(Canvas canvas,int i,int j, int k, int color , String status) {
+//
+//        Paint tempTextPlain
+//
+//
+//    }
+
+
+    String apiKey = "2d1dbe92587345da9bf698b0d221beb0";
+    public FaceServiceRestClient faceServiceRestClient = new FaceServiceRestClient(apiKey);
+
+
+
+
+
+
+
+}
