f40c22b82ace81861e593a887aa28d4de19f12de
hannah <hholl002@odu.edu>
2020-02-03 18:51:57 -0500

    commit

--- app/src/main/java/edu/odu/cs411/loqui/MainActivity.java
+++ app/src/main/java/edu/odu/cs411/loqui/MainActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d43b16d7ffe28688cc6078752a015da97f5f4d076f3bea3d84b3b7d462bad47
-size 332
+oid sha256:471b5ae094e4d22981514939db5fbe28e2ef77c7e5b7c234ec3d102a6dcf1bdc
+size 948
