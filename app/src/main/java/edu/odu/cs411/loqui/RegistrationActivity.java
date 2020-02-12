97155bd0d648786a605f2dc4bf2fa166e0d5b84d
cguil002 <cguil002@odu.edu>
2020-02-12 15:24:03 -0500

    User data is added to Firestore when a new user is created

--- app/src/main/java/edu/odu/cs411/loqui/RegistrationActivity.java
+++ app/src/main/java/edu/odu/cs411/loqui/RegistrationActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6658cc56e72b518bf7a988acbdd6d9abe73cdbe2a7e72b52bb9ed2c6f2cba7f6
-size 4379
+oid sha256:3222d76aa92e416de261e43ee206076a80a6564f7aa5a55508ff7750a5deda9b
+size 4653
