5db54fbcc98f5f9c2112657436875e84b8b7751c
zross <zross001@odu.edu>
2020-02-13 02:23:24 -0500

    Moved rewards() function to its own class. Added hardcoded feature where rewards() triggers when any three questions have been answered correctly.

---
+++ app/src/main/java/edu/odu/cs411/loqui/Rewards.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0cbb43ad8af7558946bde860b33c0a0c60bf9e7c4f5ad620f097f29dde3c6eea
+size 1441
--- app/src/main/java/edu/odu/cs411/loqui/Task3_question.java
+++ app/src/main/java/edu/odu/cs411/loqui/Task3_question.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d46eaa8f43516d9873e8f9313040fc935b757310e40a5b1df66eb12675ce34ac
-size 14293
+oid sha256:ac46c9a863be6293e0db713f572d0a03934e87d0469df3e939fcb2dd57e8179b
+size 13077
