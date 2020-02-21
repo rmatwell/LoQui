37347d20ad7292465c71d1e47fafc6754ffca96b
cguil002 <cguil002@odu.edu>
2020-02-21 12:15:56 -0500

    Added more information to Firestore Figured out how to load user data in other activities

--- app/src/main/java/edu/odu/cs411/loqui/RegistrationActivity.java
+++ app/src/main/java/edu/odu/cs411/loqui/RegistrationActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04a822a89c3455d90fa63e27dd7b44ca5255c566532c4c58627de186d1ff7233
-size 4550
+oid sha256:6a51de52ac89f25b402ff4971277a2f6ba2b3ce2f96f0f0b82d4d788f763b539
+size 5307
--- app/src/main/java/edu/odu/cs411/loqui/Task3_question.java
+++ app/src/main/java/edu/odu/cs411/loqui/Task3_question.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac46c9a863be6293e0db713f572d0a03934e87d0469df3e939fcb2dd57e8179b
-size 13077
+oid sha256:84138545b90b9906016db9ca8de6055eaf8b0eb721bbb7cd80c503e5f4ecfe86
+size 13924
