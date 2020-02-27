e9084a446863d44837fc34c696f27a30035d48e4
hannah <hholl002@odu.edu>
2020-02-27 08:55:23 -0500

    updated avatar page and added new cart for speech game

--- app/src/main/java/edu/odu/cs411/loqui/LoginActivity.java
+++ app/src/main/java/edu/odu/cs411/loqui/LoginActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6f4e01a475a8cc3d32925e5f2ca8d863eece9633c0eaa591540bc0d17b88a52
-size 3999
+oid sha256:67effa64224663e40922338f3e909e3085358303fa6699b6824868777d7bda4c
+size 3997
--- app/src/main/java/edu/odu/cs411/loqui/RegistrationActivity.java
+++ app/src/main/java/edu/odu/cs411/loqui/RegistrationActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a51de52ac89f25b402ff4971277a2f6ba2b3ce2f96f0f0b82d4d788f763b539
-size 5307
+oid sha256:7986ae041f343e968229d82268e018efb5a33b0fdcf40d9eb4c8b19213543a11
+size 5298
