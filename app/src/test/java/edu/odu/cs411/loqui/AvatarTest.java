efb42137a0defd205bd8a833958037ebff9893ce
jfarr <jfarr@cs.odu.edu>
2020-03-04 12:52:04 -0500

    Adding Avatar Test File to Repo (Will Update Later)

---
+++ app/src/test/java/edu/odu/cs411/loqui/AvatarTest.java
@@ -0,0 +1,12 @@
+package edu.odu.cs411.loqui;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AvatarTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
