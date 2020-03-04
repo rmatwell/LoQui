186721d1dd57050f761d9be6b9b593243a1b8363
jfarr <jfarr@cs.odu.edu>
2020-03-04 12:59:40 -0500

    Adding Test File for Goals and Rewards To Repo

---
+++ app/src/test/java/edu/odu/cs411/loqui/GoalsActivityTest.java
@@ -0,0 +1,8 @@
+package edu.odu.cs411.loqui;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GoalsActivityTest {
+}
---
+++ app/src/test/java/edu/odu/cs411/loqui/RewardsTest.java
@@ -0,0 +1,7 @@
+package edu.odu.cs411.loqui;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RewardsTest {
+}
