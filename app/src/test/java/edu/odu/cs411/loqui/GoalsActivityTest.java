0d84044dd31fc8f14cca551b7dba46f2e55e49c8
jfarr <jfarr@cs.odu.edu>
2020-03-04 12:59:40 -0500

    Adding Test File for Goals and Rewards To Repo

---
+++ app/src/test/java/edu/odu/cs411/loqui/GoalsActivityTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a4a4ca117ccd496aca2b34ab6c59a3cee32254ef4829ef4d7a195d7793e87825
+size 124
---
+++ app/src/test/java/edu/odu/cs411/loqui/RewardsTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4891b3c930f50af447df4841eda08cb8dbb80d4516a02d79cd2bb2418d3edfaa
+size 117
