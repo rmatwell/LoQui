4c3f64392e1750c65f1d7bc7956b111dc4235ed9
hhollowa <hhollowa@cs.odu.edu>
2020-03-25 17:52:50 -0400

    removed old java classes and xml files

--- app/src/main/java/edu/odu/cs411/loqui/Task2_1.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7779514cfb0b9fd0048addd699f92fa943a60f841e53095e8863f70b6a3e63ba
-size 6368
--- app/src/main/java/edu/odu/cs411/loqui/Task2_2.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e441144099973fb0d4513e186dfe9f785fdeb31b1a2ea6ce63411f1d009a6fd7
-size 11388
