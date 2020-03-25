a6bc056b4bf5e6f12de00a4bc5f85ef9c96460fd
ajames <ajame024@odu.edu>
2020-03-25 16:41:34 -0400

    Switched Rewards and tutorial cards in parent portal

--- app/src/main/java/edu/odu/cs411/loqui/MusicActivity.java
+++ app/src/main/java/edu/odu/cs411/loqui/MusicActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89488f2f1c1444f14f0805a48d79f9e2d4c4141b7afb5f8996557dd0ae12a8a7
-size 3658
+oid sha256:61af1552d11ddeaaad3027b71d4cf7275f0ed2f5b0d4427b2f0e0239b1d078a6
+size 3693
