48024ad748e9ac15252cb4a5c53d6b8582829b42
cguil002 <cguil002@odu.edu>
2020-03-26 03:25:43 -0400

    Changed Registration so that the name of the user in the database is the email address that they use.
    Added fields to Registration for rewardScore, which keeps track of the total score progress towards getting
    a reward and rewardLimit, which is the number of points needed to get a reward.
    
    Added code in Task3_question.java to the block of code that fires when all 3 guesses are correct. The users
    data for rewardScore is pulled from Firestore and one point is added, and then that new score is uploaded
    back into Firestore.
    
    Added code in Homepage.java that will pull the users current rewardScore from Firestore, and use that value
    to fill in the progress bar at the top of the page. I figured out why Hannah had to add a button and couldn't
    simply update the progress bar. It seems that Android does not read code in order like we are used to. Parts
    of the code were being carried out before other parts, and the order matters for this progress bar. I added
    code that delays the updating of the progress bar by 1 second to give all the other code time to load, and
    time for Firebase to be queried for the current score of the user.
    
    I added more bars to the progress bar so that we can display more than just 5 points. I have it set to 20
    for now. This can be changed later if we decide to stick with this progress bar.

--- app/src/main/java/edu/odu/cs411/loqui/Homepage.java
+++ app/src/main/java/edu/odu/cs411/loqui/Homepage.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:229e6e5d4d00578369dbdb9fdea0b40eb6eda89c1e4b84d0ee955a0f238a41f9
-size 3088
+oid sha256:c2af09c5353f1e67b1fca86e3edc0e9200e6990a5265cccd9545c000ff9ae51e
+size 5886
--- app/src/main/java/edu/odu/cs411/loqui/Task3_question.java
+++ app/src/main/java/edu/odu/cs411/loqui/Task3_question.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44c9e89ac72beaec471af64412a05af91ece08e149d639c47c9b56d6dc4e5072
-size 13695
+oid sha256:b714627366e4846f726c55ac5f5ace12db71452ddffc7d40d19856732205fbdf
+size 15578
