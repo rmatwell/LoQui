0b3b2b8bb48a22cccc50981e05244ccc4fd9841c
zross <zross001@odu.edu>
2020-03-26 07:00:07 -0400

    Goals fix.

--- app/src/main/java/edu/odu/cs411/loqui/Goals.java
+++ app/src/main/java/edu/odu/cs411/loqui/Goals.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4625f2098d595c90c487038e5b80d9d4764f06255aa96d7cd52fd0a67c5f689d
-size 6806
+oid sha256:68a679756f2b4dfab52c98d112c9a50c100493a6579179f91556deab3b89aecd
+size 6814
--- app/src/main/java/edu/odu/cs411/loqui/Task3_question.java
+++ app/src/main/java/edu/odu/cs411/loqui/Task3_question.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b714627366e4846f726c55ac5f5ace12db71452ddffc7d40d19856732205fbdf
-size 15578
+oid sha256:d1908665fd09cda0afe3ad7745ed048e18da7302c42a11ced9b7c069cfdd6398
+size 16023
