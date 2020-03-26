abfa64e6ab13f2e64bd41f610bc4dadeb5ce81a1
ajames <ajame024@odu.edu>
2020-03-26 07:27:54 -0400

    Fixed bug in Parent portal setting

--- app/src/main/java/edu/odu/cs411/loqui/ParentPortalActivity.java
+++ app/src/main/java/edu/odu/cs411/loqui/ParentPortalActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+
+import java.util.Date;
 
 public class ParentPortalActivity extends AppCompatActivity {
 
@@ -25,14 +29,44 @@ public class ParentPortalActivity extends AppCompatActivity {
 
         goalsCard = (CardView) findViewById(R.id.portal_goals);
         progressCard = (CardView) findViewById(R.id.portal_progress);
-        rewardsCard = (CardView) findViewById(R.id.portal_rewards);
+        //rewardsCard = (CardView) findViewById(R.id.portal_rewards);
         settingsCard = (CardView) findViewById(R.id.portal_settings);
         portal_backbtn = findViewById(R.id.portal_back_btn);
 
         goalsCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent it = new Intent(ParentPortalActivity.this, GoalsActivity.class);
+                Intent it = new Intent(ParentPortalActivity.this, Goals.class);
+                //game type
+                int game = 0;
+                RadioButton g_emotion = (RadioButton) findViewById(R.id.emotion_game);
+                if (g_emotion.isChecked()) {game = 0;}
+                RadioButton g_speech = (RadioButton) findViewById(R.id.speech_training);
+                if (g_speech.isChecked()) {game = 1;}
+                RadioButton g_eye = (RadioButton) findViewById(R.id.eye_contact_game);
+                if (g_eye.isChecked()) {game = 2;}
+                it.putExtra("game", game);
+                //goal type
+                int goal = 0;
+                RadioButton g_overall = (RadioButton) findViewById(R.id.correct_overall);
+                if (g_overall.isChecked()) {goal = 0;}
+                RadioButton g_streak = (RadioButton) findViewById(R.id.correct_streak);
+                if (g_streak.isChecked()) {goal = 1;}
+                RadioButton g_overallt = (RadioButton) findViewById(R.id.correct_overall_time);
+                if (g_overallt.isChecked()) {goal = 2;}
+                RadioButton g_percent = (RadioButton) findViewById(R.id.correct_percent_time);
+                if (g_percent.isChecked()) {goal = 3;}
+                it.putExtra("goal", goal);
+                //amount or percent value
+                EditText t_amount = (EditText) findViewById(R.id.amount_correct);
+                int amount = Integer.parseInt(t_amount.getText().toString());
+                it.putExtra("amount", amount);
+                //time value in seconds
+                EditText t_time = (EditText) findViewById(R.id.time);
+                int time = Integer.parseInt(t_time.getText().toString());
+                it.putExtra("time", time);
+                long timestamp = new Date().getTime()/1000;
+                it.putExtra("timestamp", timestamp);
                 startActivity(it);
             }
         });
@@ -45,13 +79,6 @@ public class ParentPortalActivity extends AppCompatActivity {
             }
         });
 
-        rewardsCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent it = new Intent(ParentPortalActivity.this, Rewards.class);
-                startActivity(it);
-            }
-        });
 
         settingsCard.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/edu/odu/cs411/loqui/SettingsActivity.java
+++ app/src/main/java/edu/odu/cs411/loqui/SettingsActivity.java
@@ -17,7 +17,6 @@ public class SettingsActivity extends AppCompatActivity {
 
     private CardView accountCard, tutorialCard, audioCard, aboutCard;
     private ImageView settings_backbtn;
-    private Button goals_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,47 +30,9 @@ public class SettingsActivity extends AppCompatActivity {
         //accountCard = (CardView) findViewById(R.id.settings_account);
         //tutorialCard = (CardView) findViewById(R.id.settings_tutorial);
         audioCard = (CardView) findViewById(R.id.settings_audio);
-        goals_btn = (Button) findViewById(R.id.create_goal);
         //aboutCard = (CardView) findViewById(R.id.settings_about);
         settings_backbtn = findViewById(R.id.settings_back_btn);
 
-        goals_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent it = new Intent(SettingsActivity.this, Goals.class);
-                //game type
-                int game = 0;
-                RadioButton g_emotion = (RadioButton) findViewById(R.id.emotion_game);
-                if (g_emotion.isChecked()) {game = 0;}
-                RadioButton g_speech = (RadioButton) findViewById(R.id.speech_training);
-                if (g_speech.isChecked()) {game = 1;}
-                RadioButton g_eye = (RadioButton) findViewById(R.id.eye_contact_game);
-                if (g_eye.isChecked()) {game = 2;}
-                it.putExtra("game", game);
-                //goal type
-                int goal = 0;
-                RadioButton g_overall = (RadioButton) findViewById(R.id.correct_overall);
-                if (g_overall.isChecked()) {goal = 0;}
-                RadioButton g_streak = (RadioButton) findViewById(R.id.correct_streak);
-                if (g_streak.isChecked()) {goal = 1;}
-                RadioButton g_overallt = (RadioButton) findViewById(R.id.correct_overall_time);
-                if (g_overallt.isChecked()) {goal = 2;}
-                RadioButton g_percent = (RadioButton) findViewById(R.id.correct_percent_time);
-                if (g_percent.isChecked()) {goal = 3;}
-                it.putExtra("goal", goal);
-                //amount or percent value
-                EditText t_amount = (EditText) findViewById(R.id.amount_correct);
-                int amount = Integer.parseInt(t_amount.getText().toString());
-                it.putExtra("amount", amount);
-                //time value in seconds
-                EditText t_time = (EditText) findViewById(R.id.time);
-                int time = Integer.parseInt(t_time.getText().toString());
-                it.putExtra("time", time);
-                long timestamp = new Date().getTime()/1000;
-                it.putExtra("timestamp", timestamp);
-                startActivity(it);
-            }
-        });
 
         audioCard.setOnClickListener(new View.OnClickListener() {
             @Override
