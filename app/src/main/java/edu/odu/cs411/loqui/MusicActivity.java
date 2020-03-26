83febab28cca3a83f60c3531aea4281e3a3a5f83
ajames <ajame024@odu.edu>
2020-03-26 00:16:24 -0400

    Added play and stop button to music setting

--- app/src/main/java/edu/odu/cs411/loqui/MusicActivity.java
+++ app/src/main/java/edu/odu/cs411/loqui/MusicActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61af1552d11ddeaaad3027b71d4cf7275f0ed2f5b0d4427b2f0e0239b1d078a6
-size 3693
+oid sha256:8ca92730f9de109f5c7c0a243a2309751f39ebdf1881243a3b6afc8b6cb039a3
+size 1682
