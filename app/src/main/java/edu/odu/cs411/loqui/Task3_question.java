8cc857f19e869f42dcb6d0939c1a3f62c1bfc07f
cguil002 <cguil002@odu.edu>
2020-04-08 19:54:53 -0400

    Added in progress tracking data. When a game is played, you can now use the FirestoreWorker to record the score along with the date so that we can track the scores later.

--- app/src/main/java/edu/odu/cs411/loqui/Task3_question.java
+++ app/src/main/java/edu/odu/cs411/loqui/Task3_question.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0069dfe53ce75abc3089d4551b4669241d9520ed21fdee49378874be1d90eb98
-size 13463
+oid sha256:cc616ad9f9f69ee2df4cacdd60265fc1415287d083c8540f84f5f166a2ccfa64
+size 13508
