50967cb61bff1a880c3887f7431db6ba7e0f9ceb
cguil002 <cguil002@odu.edu>
2020-04-08 15:46:58 -0400

    Added code that checks for if the rewardScore is at 20 before adding points, and won't add more points than the max of 20.
    Former-commit-id: 9e15a4eafa4641df01a36eff45efa6cc49d3ff1d

--- app/src/main/java/edu/odu/cs411/loqui/FirestoreWorker.java
+++ app/src/main/java/edu/odu/cs411/loqui/FirestoreWorker.java
@@ -19,6 +19,7 @@ public class FirestoreWorker
     String userID;
     FirebaseFirestore db;
     String TAG = "Firestore Worker";
+    static boolean rewardFlag;
 
     FirestoreWorker()
     {
@@ -26,6 +27,49 @@ public class FirestoreWorker
         user = firebaseAuth.getCurrentUser();
         db = FirebaseFirestore.getInstance();
         userID = user.getEmail();
+    }
+
+    public void checkForReward()
+    {
+        DocumentReference userRef = db.collection("users").document(userID);
+
+        userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful())
+                {
+                    DocumentSnapshot userData = task.getResult();
+
+                    if(userData.exists())
+                    {
+                        Log.d(TAG, "DocumentSnapshot data: " + userData.getData());
+                    }
+                    else {
+                        Log.d(TAG, "No such document userID = " + userID);
+                    }
+                    double dbScore = userData.getDouble("rewardScore");
+
+                    if (dbScore >= 20)
+                    {
+                        rewardFlag = true;
+                    }
+                    else
+                    {
+                        rewardFlag = false;
+                    }
+                }
+                else
+                {
+                    Log.d(TAG, "get fail with ", task.getException());
+                }
+            }
+        })
+
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                    }
+                });
     }
 
     public void addToRewardScore(int amount)
@@ -46,9 +90,29 @@ public class FirestoreWorker
                     else {
                         Log.d(TAG, "No such document userID = " + userID);
                     }
-                    double dbScore = userData.getDouble("rewardScore");
-                    dbScore = dbScore + amount;
-                    userRef.update("rewardScore", dbScore);
+                    checkForReward();
+
+                    if(rewardFlag == true)
+                    {
+                        //do not increment over 20
+                    }
+                    else
+                    {
+                        double dbScore = userData.getDouble("rewardScore");
+
+                        //If the amount to be added would make rewardScore 20 or more
+                        if(dbScore + amount >= 20)
+                        {
+                            double difference = (dbScore + amount) - 20;
+                            dbScore = dbScore + difference;
+                            userRef.update("rewardScore", dbScore);
+                        }
+                        else
+                        {
+                            dbScore = dbScore + amount;
+                            userRef.update("rewardScore", dbScore);
+                        }
+                    }
                 }
                 else
                 {
