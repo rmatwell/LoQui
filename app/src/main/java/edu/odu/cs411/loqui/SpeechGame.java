8dc37ae503b6e4d9a168039398f9385642d5ea6b
rmatwell <richard.atwell@hotmail.com>
2020-04-08 20:46:44 -0400

    Update SpeechGame.java, activity_speech_game.xml

--- app/src/main/java/edu/odu/cs411/loqui/SpeechGame.java
+++ app/src/main/java/edu/odu/cs411/loqui/SpeechGame.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15fe74ebd88ba55f73d38cb7c03df873ae24ea66602ac31bac76ee86ecd4f337
-size 3123
+oid sha256:69752ba745c56bd8ebcad9f6f8ca92ee24b1bded6e816e2e9e13fb858c8613ad
+size 3918
