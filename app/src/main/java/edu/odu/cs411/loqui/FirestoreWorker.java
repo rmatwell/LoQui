6029c08ce5aaeacc399f29d5cdd05364f02a1d55
cguil002 <cguil002@odu.edu>
2020-04-12 15:50:29 -0400

    Added a way to create an account with pre seeded data for the emotion game to demo progress tracking. You must enter "ProgressTest" in the username field, and anything else for the other fields.

--- app/src/main/java/edu/odu/cs411/loqui/FirestoreWorker.java
+++ app/src/main/java/edu/odu/cs411/loqui/FirestoreWorker.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c1e0554e55bd5a171554bbb9d72a3816793c19a6d6988c01f1b7da4c9623080
-size 14268
+oid sha256:21bb46f97bab65e34b73d907ecd9f5154f178c11a1adc06e8ab8dbf29688aeae
+size 14896
