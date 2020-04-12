421635e21c30635515f823565e55922f7a1874ee
zross <zross001@odu.edu>
2020-04-12 16:47:12 -0400

    Goals fix. Finally works.

--- app/src/main/java/edu/odu/cs411/loqui/Goals.java
+++ app/src/main/java/edu/odu/cs411/loqui/Goals.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2a2610be15caf497121a989486de831476b74e6fc2cddf15cdc7577dc353411
-size 8492
+oid sha256:8c96988c9ac00b696320daa1d82ac02b4451259d638314d5d5ccba4faaaabfc4
+size 8540
