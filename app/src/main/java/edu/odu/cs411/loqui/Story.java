4f7f44386fece6afd4c04236886364f170df7e22
jaker <jaker001@odu.edu>
2020-04-12 19:39:06 -0400

    Added Google Vision library
    Former-commit-id: 17459238fa14bf0f7edbcdb7f57d4a8cf1349e3f

--- app/src/main/java/edu/odu/cs411/loqui/Story.java
+++ app/src/main/java/edu/odu/cs411/loqui/Story.java
@@ -1,18 +1,35 @@
 package edu.odu.cs411.loqui;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.PixelFormat;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.VideoView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class Story extends AppCompatActivity {
 
+    CameraSource camera;
+    VideoView video;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +39,22 @@ public class Story extends AppCompatActivity {
         } catch (NullPointerException e) {
         }
         setContentView(R.layout.activity_story);
-        Button play = (Button) findViewById(R.id.button);
         getWindow().setFormat(PixelFormat.UNKNOWN);
-        VideoView video = (VideoView) findViewById(R.id.videoView);
-        String path = "android.resource://edu.odu.cs411.loqui/" + R.raw.avatar1;
-        Uri uri = Uri.parse(path);
-        video.setVideoURI(uri);
-        video.requestFocus();
-        video.start();
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+            Toast.makeText(this, "Grant Permission and restart app", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            video = (VideoView) findViewById(R.id.videoView);
+            String path = "android.resource://edu.odu.cs411.loqui/" + R.raw.avatar1;
+            Uri uri = Uri.parse(path);
+            video.setVideoURI(uri);
+            video.requestFocus();
+            video.start();
+
+
+        }
     }
 
 }
