e1242a1145b07bfb15809d5bcbb2b836992a0fc5
cguil002 <cguil002@odu.edu>
2020-04-13 17:24:34 -0400

    Reworked the Goals class and added Firestore capabilities to it.

--- app/src/main/java/edu/odu/cs411/loqui/Goals.java
+++ app/src/main/java/edu/odu/cs411/loqui/Goals.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf1a416abc2a5b6fea2a2449f4e41a290ac04bec274a532326f6bd608efb4bc7
-size 8539
+oid sha256:1c0193bff8ba8cfdc4d29f08a73e730f02d22507daf342ca47fc9f1238351e64
+size 11111
--- app/src/main/java/edu/odu/cs411/loqui/Task3_question.java
+++ app/src/main/java/edu/odu/cs411/loqui/Task3_question.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:768986922f357a90925e6f15706fa28a5006dd25c5dbaf4f24bef211f8c2a584
-size 13632
+oid sha256:0e642dcc349acb9a22d0d235f2b9cc50bdf3715e82eaa7611b8fb67ef23417a5
+size 14329
