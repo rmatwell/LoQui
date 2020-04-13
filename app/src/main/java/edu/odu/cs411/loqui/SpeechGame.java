b83b1724a03c95d8c98ef6b4cbddc91703a462e0
rmatwell <richard.atwell@hotmail.com>
2020-04-12 23:58:13 -0400

    Update SpeechGame.java, migrate to androidX

--- app/src/main/java/edu/odu/cs411/loqui/SpeechGame.java
+++ app/src/main/java/edu/odu/cs411/loqui/SpeechGame.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b721d9ab2291d603f62946db16131ca7d3e03b694e324af817bf520808c1871
-size 8852
+oid sha256:9c3c0e8bb7ac631d5a4557cff59307bc3701b0701177ae0210f3560d8aa46cdf
+size 9499
