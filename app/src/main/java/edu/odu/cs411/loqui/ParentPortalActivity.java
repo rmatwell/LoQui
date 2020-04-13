3ae6c2bbc7655018439b2117fd4a9966284880fe
ajames <ajame024@odu.edu>
2020-04-13 16:57:11 -0400

    Added cards to Progress setting UI in parent portal

--- app/src/main/java/edu/odu/cs411/loqui/ParentPortalActivity.java
+++ app/src/main/java/edu/odu/cs411/loqui/ParentPortalActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8004bc9dd2836b3a0d716589faccec04402b3e3146f193a1f34c1fb8d94d76e6
-size 2572
+oid sha256:b6fef9dd7f88cbd0eb6542fcfead7d53a691ea98005ae5b8d7bb7c4f3ffdb210
+size 2565
