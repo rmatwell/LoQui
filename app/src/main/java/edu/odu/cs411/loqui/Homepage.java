e06a10181ef502bd980a7b6e5063f17efd310875
hannah <hholl002@odu.edu>
2020-04-13 22:06:41 -0400

    commit

--- app/src/main/java/edu/odu/cs411/loqui/Homepage.java
+++ app/src/main/java/edu/odu/cs411/loqui/Homepage.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d84f9f9ab01090015ffbd77092b9b7cb104ad014e7e7ab43020cc27c11474a0f
-size 3280
+oid sha256:65ed299a69e96f20a34332b78e6ae56b4ca0eb67272a02db552626e0420e0d43
+size 3278
