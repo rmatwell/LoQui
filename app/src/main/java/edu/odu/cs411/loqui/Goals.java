d0ba0638ac5bbd33c677793b5192e6483913865f
zross <zross001@odu.edu>
2020-04-16 03:28:11 -0400

    Goals tweaks.

--- app/src/main/java/edu/odu/cs411/loqui/Goals.java
+++ app/src/main/java/edu/odu/cs411/loqui/Goals.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c0193bff8ba8cfdc4d29f08a73e730f02d22507daf342ca47fc9f1238351e64
-size 11111
+oid sha256:a09a330dbbcd92e350c9e263e9edc1b49364992e3cfce94227b6a5184a484b9e
+size 12380
