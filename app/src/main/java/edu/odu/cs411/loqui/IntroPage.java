81c0eaebfa14cfa0cc577253384f1f96b5c1f967
hannah <hholl002@odu.edu>
2020-04-16 15:06:36 -0400

    commit

--- app/src/main/java/edu/odu/cs411/loqui/IntroPage.java
+++ app/src/main/java/edu/odu/cs411/loqui/IntroPage.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df6c64f5232ae98df4c851e3690a7ee8ada43a0d5e3df8b3415eaa36e484ca68
-size 1744
+oid sha256:e728e865f2ca56c3a866244211f71ae9f0407bbf69939a39c083070e5698efc3
+size 1742
