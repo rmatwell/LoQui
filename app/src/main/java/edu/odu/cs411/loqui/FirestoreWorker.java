ff3c90c4ee4eac27b5c62b229599326a99e1712a
cguil002 <cguil002@odu.edu>
2020-04-17 20:41:34 -0400

    Added in more goal functionality and pop ups that inform the user of when a goal is earned, and when a reward is earned. The progress bar and rewardScore now reset when you reach 20. The 5 points from goals are being added successfully now, and the completed goal is being removed. There is an issue where when a goal is completed and the pop up happens, it is closed within 2 sec not giving much time to see it. Ive got the issue narrowed down to when the goal is being deleted from Firestore but don't have a solution yet. It isn't breaking the app though.

--- app/src/main/java/edu/odu/cs411/loqui/FirestoreWorker.java
+++ app/src/main/java/edu/odu/cs411/loqui/FirestoreWorker.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:774a77fe137a9da081475d8c5b83e07f2f4c52f17d4ebdaf4652164efc8b5b66
-size 41426
+oid sha256:2e1a2a0809011f3711c94559322bfac8607d6b9d0fdaa4db9524cf37ca22d581
+size 41418
