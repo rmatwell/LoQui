d5ad37dcf9a161127fe0707b24c1ff73e078591e
jaker <jaker001@odu.edu>
2020-04-19 18:51:25 -0400

    Fixed boolean values for chronometer

--- app/src/main/java/edu/odu/cs411/loqui/Story.java
+++ app/src/main/java/edu/odu/cs411/loqui/Story.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97e4a86e32c7bd7513b5b36b09c8b3fcb6d2d98815fbf33dec7c5b6ae72e6407
-size 5978
+oid sha256:fe211f32f5a40ba4eeaaee7bb086b8a6e8ce1ed6c0b4d372a89d6f33a8cb60e0
+size 6019
