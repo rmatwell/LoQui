dd02a895d3f3da6d67e0625ac2728895c73b08a5
cguil002 <cguil002@odu.edu>
2020-04-19 13:13:04 -0400

    Made some changes to the goal and reward announcements in the speech game because it wasn't updating properly.

--- app/src/main/java/edu/odu/cs411/loqui/SpeechGame.java
+++ app/src/main/java/edu/odu/cs411/loqui/SpeechGame.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e673392cfb5b522e525b3c7207c12ea6bf16add923897792e879daad7daa5ae
-size 10433
+oid sha256:41e759307ab958dc07233194bb39978367dd95de49104e6323ceb8f5cbfb7153
+size 10728
