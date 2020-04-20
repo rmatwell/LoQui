fcfd09969373a8b267215e700d4c4affce1c20a6
cguil002 <cguil002@odu.edu>
2020-04-20 13:50:45 -0400

    Added in progress tracking and reward scoring to the Eye Contact Game.

--- app/src/main/java/edu/odu/cs411/loqui/FirestoreWorker.java
+++ app/src/main/java/edu/odu/cs411/loqui/FirestoreWorker.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c692ebd777bc2d4ba2867ee273c050f1d34703d272f605bfcc05b0dbe09b471
-size 43137
+oid sha256:83f081585c8d7981a2357b55d8f2f2f98c494dbc4d4717d45c5efa6f782a5bb4
+size 43140
