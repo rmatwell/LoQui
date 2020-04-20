a49c47e97ae72cf0b897faf9e23c01988e4f2a4e
jaker <jaker001@odu.edu>
2020-04-20 00:10:28 -0400

    Added scoring functionality.

--- app/src/main/java/edu/odu/cs411/loqui/Story.java
+++ app/src/main/java/edu/odu/cs411/loqui/Story.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:848174eaff66856e8d29c95d6c765028b5bbca15b2ae3666179955882e2e208d
-size 6404
+oid sha256:4c731e53c408fceb23de7eb5ce0fe6ca09b6d701560c7a00dd479d7a700331e4
+size 8375
