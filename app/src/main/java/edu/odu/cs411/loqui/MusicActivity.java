67d6097bf7665c6036712366e6ce2cd05f10fe11
ajames <ajame024@odu.edu>
2020-04-20 16:29:31 -0400

    Replaced settings card with audio card and updated related UI

--- app/src/main/java/edu/odu/cs411/loqui/MusicActivity.java
+++ app/src/main/java/edu/odu/cs411/loqui/MusicActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9df602016b95178d0bd5cef9ba3f500169d83da4ec7645edb0fbc89e815c40d
-size 1765
+oid sha256:8be60ae1e03773c921e5295228714d8ab066e78926048057c341e52d460c1b88
+size 1769
--- app/src/main/java/edu/odu/cs411/loqui/SettingsActivity.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:21911c37c9668dd4992f62800d577c7c02612265b6d5951f57a565023bcc6346
-size 1624
