824da1da516472dc2aeb6cf439999321c0d0d488
jaker <jaker001@odu.edu>
2020-04-20 18:47:08 -0400

    Remove unnecessary toast messages in Story.

--- app/src/main/java/edu/odu/cs411/loqui/Story.java
+++ app/src/main/java/edu/odu/cs411/loqui/Story.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c731e53c408fceb23de7eb5ce0fe6ca09b6d701560c7a00dd479d7a700331e4
-size 8375
+oid sha256:a8e49febd4401cd80655668590afd5415738d96a5673272a61ec90d0cef15fc1
+size 8379
