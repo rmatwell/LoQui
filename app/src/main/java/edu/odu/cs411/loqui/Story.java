e7a00be34b435f40684a3d5c7084267fe9d4a87b
jaker <jaker001@odu.edu>
2020-04-21 13:31:58 -0400

    Fixed score formatting in Toast message

--- app/src/main/java/edu/odu/cs411/loqui/Story.java
+++ app/src/main/java/edu/odu/cs411/loqui/Story.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97085eb248b7e859306c644a11f6500d26ccdcbbeb84dd50bd7f55cdfbf06c4e
+oid sha256:e950f5b5dd533333d1ac6e731c775b68ba42ca6ef54194c40b31b1f8c3257ba3
 size 9217
