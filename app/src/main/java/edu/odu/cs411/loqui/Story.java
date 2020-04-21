44e6d941455a6d90dfcc01bf0ee034271aac32dc
jaker <jaker001@odu.edu>
2020-04-21 12:54:06 -0400

    Added imageview that shows eye contact detection status

--- app/src/main/java/edu/odu/cs411/loqui/Story.java
+++ app/src/main/java/edu/odu/cs411/loqui/Story.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5e3a01f7b9060b865a0b8a693b2eeac7901058b1be3db471ba35da7b196d5a6
-size 8736
+oid sha256:97085eb248b7e859306c644a11f6500d26ccdcbbeb84dd50bd7f55cdfbf06c4e
+size 9217
