143cd2985fef443441dc973be54e1e54cc72f7d7
jfarr <jfarr001@odu.edu>
2020-05-12 12:23:38 -0400

    Removed "All Games" Option in Goals by Commenting Block out

--- app/src/main/java/edu/odu/cs411/loqui/Goals.java
+++ app/src/main/java/edu/odu/cs411/loqui/Goals.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2064e8d7a4e8e56e1ae66f922150de37e5380a80e5165bc1c9f8450eff90f862
-size 12751
+oid sha256:23e4acd55e031ecf2f2fb5bc1ec4445e53ce9e0beab7de7ee1b6d928268b851e
+size 12757
