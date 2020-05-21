3608e400690eb47f86aea678710efe4561b988d5
jfarr <jfarr@cs.odu.edu>
2020-05-21 14:36:51 -0400

    Implemented features where video pauses/resumes depending on video status (WORK IN PROGRESS)

--- app/src/main/java/edu/odu/cs411/loqui/Story.java
+++ app/src/main/java/edu/odu/cs411/loqui/Story.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:580de2bf6801af02a8ff0e77b1dda5294f4c2bc97b60fcd806e806013c85ce55
-size 9533
+oid sha256:c36cb4d407951cd598645b0ec06bb955adfdd920986b101900d00ee4dc57ec1e
+size 9877
