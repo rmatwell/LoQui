04304ea350715a2a18ea3ead4b340c8281037152
jfarr <jfarr@cs.odu.edu>
2020-05-28 22:02:18 -0400

    Implemented Eye Contact Score to be calculated considering both video's end and interruption

--- app/src/main/java/edu/odu/cs411/loqui/Goals.java
+++ app/src/main/java/edu/odu/cs411/loqui/Goals.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23e4acd55e031ecf2f2fb5bc1ec4445e53ce9e0beab7de7ee1b6d928268b851e
-size 12757
+oid sha256:b9039af97e3d38fc26bbc96df5f5199ecb3a8d946b236ce34b0110571c26b8e9
+size 14336
--- app/src/main/java/edu/odu/cs411/loqui/Story.java
+++ app/src/main/java/edu/odu/cs411/loqui/Story.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bc15b8885e603bd6daa7338b798bee3f32bee8f853c6a6408acd90e5e62ee33
-size 11302
+oid sha256:68996aa466da93c0212e982234811cc43bf81329e06161273fd7f8ff28785ca4
+size 11592
